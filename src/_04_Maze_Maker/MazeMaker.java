34a5744f3aa116cbf21fea16a266a0f8f825e313
Isaacpants <dave@boyermoving.com>
2020-05-26 18:16:19 -0700

    finished

--- src/_04_Maze_Maker/MazeMaker.java
+++ src/_04_Maze_Maker/MazeMaker.java
@@ -4,76 +4,123 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Stack;
 
+public class MazeMaker {
 
-public class MazeMaker{
-	
 	private static int width;
 	private static int height;
-	
+
 	private static Maze maze;
-	
+
 	private static Random randGen = new Random();
 	private static Stack<Cell> uncheckedCells = new Stack<Cell>();
-	
-	
-	public static Maze generateMaze(int w, int h){
+
+	public static Maze generateMaze(int w, int h) {
 		width = w;
 		height = h;
 		maze = new Maze(width, height);
+
+		// 4. select a random cell to start
 		
-		//4. select a random cell to start
+int x = randGen.nextInt(maze.getWidth());
+int y = randGen.nextInt(maze.getHeight());
+		// 5. call selectNextPath method with the randomly selected cell
+		selectNextPath(maze.getCell(x, y));
 		
-		
-		//5. call selectNextPath method with the randomly selected cell
-		
-		
+
 		return maze;
 	}
 
-	//6. Complete the selectNextPathMethod
+	// 6. Complete the selectNextPathMethod
 	private static void selectNextPath(Cell currentCell) {
-		//A. mark cell as visited
+		
+		// A. mark cell as visited
+		currentCell.setBeenVisited(true);
+		// B. Get an ArrayList of unvisited neighbors using the current cell and the
+		// method below
+		ArrayList<Cell> unvisited = getUnvisitedNeighbors(currentCell);
+		//int r = randGen.nextInt(unvisited.size());
 
-		//B. Get an ArrayList of unvisited neighbors using the current cell and the method below
-		
-		//C. if has unvisited neighbors,
-		
-			//C1. select one at random.
-			
-			//C2. push it to the stack
-		
-			//C3. remove the wall between the two cells
+		// C. if has unvisited neighbors,
+		if (unvisited.size() >= 1) {
+			int r = randGen.nextInt(unvisited.size());
+			// C1. select one at random.
+			// unvisited.get(r);
+			// C2. push it to the stack
+			uncheckedCells.push(unvisited.get(r));
+			// C3. remove the wall between the two cells
+			removeWalls(currentCell,unvisited.get(r));
+			// C4. make the new cell the current cell and mark it as visited
+			currentCell = unvisited.get(r);
+			//currentCell.setBeenVisited(true);
+			// C5. call the selectNextPath method with the current cell
+			selectNextPath(currentCell);
+		}
 
-			//C4. make the new cell the current cell and mark it as visited
-		
-			//C5. call the selectNextPath method with the current cell
-			
-			
-		//D. if all neighbors are visited
-		
-			//D1. if the stack is not empty
-			
+		// D. if all neighbors are visited
+		if (unvisited.size() <= 0) {
+			// D1. if the stack is not empty
+			if (!uncheckedCells.isEmpty()) {
 				// D1a. pop a cell from the stack
-		
+				currentCell = uncheckedCells.pop();
 				// D1b. make that the current cell
-		
+
 				// D1c. call the selectNextPath method with the current cell
-				
-			
-		
+				selectNextPath(currentCell);
+			}
+		}
+
 	}
 
-	//7. Complete the remove walls method.
-	//   This method will check if c1 and c2 are adjacent.
-	//   If they are, the walls between them are removed.
+	// 7. Complete the remove walls method.
+	// This method will check if c1 and c2 are adjacent.
+	// If they are, the walls between them are removed.
 	private static void removeWalls(Cell c1, Cell c2) {
-		
+		if (c1.getX() == c2.getX()) {
+			if (c1.getY() > c2.getY()) {
+				c1.setNorthWall(false);
+				c2.setSouthWall(false);
+			} else {
+				c1.setSouthWall(false);
+				c2.setNorthWall(false);
+			}
+		} else {
+			if (c1.getX() > c2.getX()) {
+				c1.setWestWall(false);
+				c2.setEastWall(false);
+			} else {
+				c1.setEastWall(false);
+				c2.setWestWall(false);
+			}
+		}
 	}
-	
-	//8. Complete the getUnvisitedNeighbors method
-	//   Any unvisited neighbor of the passed in cell gets added
-	//   to the ArrayList
+
+	// 8. Complete the getUnvisitedNeighbors method
+	// Any unvisited neighbor of the passed in cell gets added
+	// to the ArrayList
 	private static ArrayList<Cell> getUnvisitedNeighbors(Cell c) {
-		return null;
+		ArrayList<Cell> unvisited = new ArrayList<Cell>();
+
+		if (( c.getX() > 0) && !(maze.getCell(c.getX() - 1, c.getY()).hasBeenVisited())) {
+
+			unvisited.add(maze.getCell(c.getX() - 1, c.getY()));
+
+		}
+		if (c.getX() < width - 1 && !(maze.getCell(c.getX() + 1, c.getY()).hasBeenVisited())) {
+
+			unvisited.add(maze.getCell(c.getX() + 1, c.getY()));
+
+		}
+		if ((c.getY() > 0 && !(maze.getCell(c.getX(), c.getY() - 1).hasBeenVisited()))) {
+
+			unvisited.add(maze.getCell(c.getX(), c.getY() - 1));
+
+		}
+		if (c.getY() < height - 1 && !(maze.getCell(c.getX(), c.getY() + 1).hasBeenVisited())) {
+			
+			unvisited.add(maze.getCell(c.getX(), c.getY()+1));
+			
+		}
+
+		return unvisited;
 	}
 }
--- src/extras/the_wrong_way_cow/TheWrongWayCow.java
+++ src/extras/the_wrong_way_cow/TheWrongWayCow.java
@@ -41,12 +41,101 @@
 
 package extras.the_wrong_way_cow;
 
+import java.util.HashMap;
+
 public class TheWrongWayCow {
 
-    public static int[] findWrongWayCow(final char[][] field) {
-        // Fill in the code to return the x,y coordinate position of the
-        // head (letter 'c') of the wrong way cow!
-        
-        return null;
-    }
+	static HashMap<String, int[]> location = new HashMap<String, int[]>();
+	
+	static int x = 0;
+	static int y = 0;
+
+	public static int[] findWrongWayCow(final char[][] field) {
+		 int counter1 = 0;
+		 int counter2 = 0;
+		 int counter3 = 0;
+		 int counter4 = 0;
+		// Fill in the code to return the x,y coordinate position of the
+		// head (letter 'c') of the wrong way cow!
+		for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field[i].length; j++) {
+				if (field[i][j] == 'o') {
+
+					if (!(i == 0)) {
+						// left to right
+						if ((field[i - 1][j] == 'c' && field[i + 1][j] == 'w')) {
+							location.put("east", new int[] { j,i-1});
+							counter1++;
+
+						}
+					}
+					if ((j != field[i].length - 1)) {
+						if ((j != 0)) {
+							// bottom to top
+							if ((field[i][j + 1] == 'c' && field[i][j - 1] == 'w')) {
+								location.put("north", new int[] { j + 1 ,i });
+								counter2++;
+
+							}
+						}
+					}
+					if (!(j == 0)) {
+						// top to bottom
+						if ((field[i][j - 1] == 'c' && field[i][j + 1] == 'w')) {
+							location.put("south", new int[] { j - 1 ,i  });
+
+							counter3++;
+
+						}
+					}
+
+					if (!(i == field.length - 1)) {
+						// right to left
+						if ((field[i + 1][j] == 'c' && field[i - 1][j] == 'w')) {
+							location.put("west", new int[] {j, i + 1});
+							counter4++;
+
+						}
+					}
+
+				} // counterCounter();
+
+			}
+
+		}
+		if (counter1 == 1) {
+			return location.get("east");
+		}
+		if (counter2 == 1) {
+			return location.get("north");
+		}
+		if (counter3 == 1) {
+			return location.get("south");
+		}
+		if (counter4 == 1) {
+			return location.get("west");
+		}
+		System.out.println(counter1);
+		System.out.println(counter2);
+		System.out.println(counter3);
+		System.out.println(counter4);
+//		int i = 0;
+//		if(counter.get(i)==0) {
+//			 i++;
+//			 
+//		}
+
+		return null;
+
+	}
+//static void counterCounter() {
+//		
+//		for (int i = 0; i < counter.size(); i++) {
+//			if(counter.get(i)>counter.get(i++)) {
+//				int k = counter.get(i);
+//				counter.add(i, counter.get(i+1));
+//				counter.add(i+1, k);
+//			}
+//		}
+//	}
 }
